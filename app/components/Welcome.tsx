c2dca192193c885db0353a6f76ebb7ec68e435b4
PhongThanh0907 <phong.thanh971997@gmail.com>
2023-05-14 18:36:45 +0700

    first-build

---
+++ app/components/Welcome.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+const Welcome = (props: Props) => {
+  return (
+    <div className="text-white">
+      <h1>Welcome</h1>
+    </div>
+  );
+};
+
+export default Welcome;
