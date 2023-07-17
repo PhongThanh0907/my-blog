f7f4720e6122047baade4a9444d3f496c7423779
PhongThanh0907 <phong.thanh971997@gmail.com>
2023-07-17 14:06:47 +0700

    update code

--- app/my-portfolio/page.tsx
+++ app/my-portfolio/page.tsx
@@ -1,41 +1,31 @@
 "use client";
 
 import React from "react";
-import { motion } from "framer-motion";
 
 import Header from "./Header";
 import Banner from "./Banner";
 import OverView from "./OverView";
-import FirstLoading from "./FirstLoading";
 import WorkExperience from "./WorkExperience";
 import Technologies from "./Technologies";
 import Project from "./Project";
 import Contact from "./Contact";
 import ToasterProvider from "../providers/ToasterProvider";
+import ParticlesContainer from "../components/particles/ParticlesContainer";
 
-type Props = {};
-
-const MyPortfolioPage = (props: Props) => {
+const MyPortfolioPage = () => {
   return (
-    <main className="min-h-screen bg-black">
+    <main className="min-h-screen bg-black relative">
       <>
-        <FirstLoading />
-        <motion.div
-          initial={{
-            opacity: 0,
-          }}
-          transition={{ delay: 1.5 }}
-          whileInView={{ opacity: 1 }}
-        >
-          <ToasterProvider />
-          <Header />
-          <Banner />
-          <OverView />
-          <WorkExperience />
-          <Technologies />
-          <Project />
-          <Contact />
-        </motion.div>
+        <ParticlesContainer />
+
+        <ToasterProvider />
+        <Header />
+        <Banner />
+        <OverView />
+        <WorkExperience />
+        <Technologies />
+        <Project />
+        <Contact />
       </>
     </main>
   );
